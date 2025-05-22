facc74631b6fcb315664ca06f76cfb804ab2bb2d
Orlen-07 <adonayrafaelano17@gmail.com>
2025-05-21 23:54:55 -0600

    mi primer commit

---
+++ Clase 2/Ejercicio3_Clase2.py
@@ -0,0 +1,16 @@
+'''
+Clase:        2
+Tema:         Bloque condicional
+Ejercicio:    3
+Descripción:  2.4.1. El número mágico
+
+Autor:        Enrique Hernández
+Fecha:        2025-05-21
+Estado:       [ Terminado ]
+'''
+
+n=int(input())
+if n%7==0 and n%5!=0:
+  print("Mágico")
+else:
+  print("Normal")
